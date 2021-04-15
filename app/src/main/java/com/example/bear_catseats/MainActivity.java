253095f37a759e58a69bf1d198fc4cb2a4c4359a
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2021-04-15 14:09:43 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/bear_catseats/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bear_catseats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
